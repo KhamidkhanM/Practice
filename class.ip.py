005b852b1cf471f01fb5a6c3a78cd723b53b072f
KhamidkhanM <HoshimalievHamidxonn@gmail.com>
2026-05-14 13:36:45 +0900

    feat: 36-38 themes done

---
+++ class.ip.py
@@ -0,0 +1,100 @@
+'''
+CLASS deep diving
+    (1) ENCAPSULATION
+    (2) INHERITENCE
+    (3) POLIMORPHISM
+'''
+
+print("===== INHERITENCE =====")
+# PARENT > CHILD
+
+
+class Animal:  # Parent
+    # state
+    description = "The class creates animals"
+
+    # constructor
+    def __init__(self, voice):
+        self.voice = voice
+
+    # method
+    def make_voice(self):
+        print(f"the animal can make voice: {self.voice}")
+
+
+class Dog(Animal):  # Child
+    # state
+
+    # constructor
+    def __init__(self, name, sound, voice):
+        self.name = name
+        self.sound = sound
+        super().__init__(voice)
+
+    # method
+    def introduce(self):
+        print(f"{self.name} says: {self.sound}-{self.sound}")
+
+    def protect(self):
+        print("Yes, I can protect you!")
+
+
+class Cat(Animal):  # Child
+    # state
+
+    # constructor
+    def __init__(self, name, sound, voice):
+        self.name = name
+        self.sound = sound
+        super().__init__(voice)
+
+    # method
+    def introduce(self):
+        print(f"{self.name} says: {self.sound}-{self.sound}")
+
+    def play(self):
+        pass
+
+
+class Fish(Animal):  # Child
+    pass
+
+
+dog = Dog("Rex", "wow", True)
+cat = Cat("Tom", "myeow", True)
+fish = Fish("Nemo", "ZzZ", False)
+
+dog.introduce()
+cat.introduce()
+fish.introduce()
+
+print("------")
+dog.make_voice()
+fish.make_voice()
+
+print(Animal.description)
+print(Dog.description)
+
+print(dog.voice, fish.voice)
+print("dog.status:", dog._status)
+print("cat.status:", cat._status)
+
+
+print("===== POLIMORPHISM =====")
+
+dog.make_voice()
+fish.make_voice()
+
+print("------")
+# fish > Fish > Animal > object
+a = isinstance(fish, Fish)
+b = isinstance(fish, Animal)
+c = isinstance(fish, object)
+d = isinstance("MIT", object)
+result = a and b and c and d
+print(f"the result: {result}")
+
+# Fish > Animal > object
+data1 = issubclass(Fish, Animal)
+data2 = issubclass(Animal, object)
+print("data:", data1, data2)
